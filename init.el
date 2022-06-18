ca76bb5c6edefb86d5c0f5433333a7684c5ddbc0
Jack Rose <user@jackrose.co.nz>
2022-06-18 20:06:20 +1000

    misc emacs config

--- init.el
+++ init.el
@@ -19,30 +19,14 @@
 (setq use-package-always-ensure t)
 
 ;; Automatically tangle our Emacs.org config file when we save it
-(defun efs/org-babel-tangle-config ()
+(defun jdr/org-babel-tangle-config ()
   (when (string-equal (file-name-directory buffer-file-name)
                       (file-name-directory user-init-file))
     ;; Dynamic scoping to the rescue
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
 
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
-
-;; NOTE: The first time you load your configuration on a new machine, you'll
-;; need to run the following command interactively so that mode line icons
-;; display correctly:
-
-;; M-x all-the-icons-install-fonts
-
-;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
-
-;; visual stuff
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(set-default 'truncate-lines t)
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'jdr/org-babel-tangle-config)))
 
 ;; theme
 (setq modus-themes-mode-line '(accented 3d padded)
@@ -65,12 +49,31 @@
 ;; Load the light theme by default
 (load-theme 'modus-operandi t)
 
+;; NOTE: The first time you load your configuration on a new machine, you'll
+;; need to run the following command interactively so that mode line icons
+;; display correctly:
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; visual stuff
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(set-default 'truncate-lines t)
+
 ;; base font setup 
 (setq font-use-system-font t)
 (set-face-attribute 'default nil :height 110)
 (recentf-mode t)
-
-(use-package all-the-icons)
 
 (setq user-emacs-directory "~/.cache/emacs")
 (use-package no-littering)
@@ -78,7 +81,6 @@
 ;; auto save files in the same path as it uses for sessions
 (setq auto-save-file-name-transforms
       `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-
 
 ;; Enable line numbering in `prog-mode'
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
@@ -221,17 +223,10 @@
 ;; Since I let evil-mode take over C-u for buffer scrolling, I need to re-bind
 ;; the universal-argument command to another key sequence
 (global-set-key (kbd "C-M-u") 'universal-argument)
-;; stop C-<direction> keys doing weird stuff
+;; stop C-<direction> keys doing weird stuff, none of it is very useful to me, and I'm used to vims way
 (global-set-key (kbd "C-j") nil)
 (global-set-key (kbd "C-k") nil)
 (global-set-key (kbd "C-l") nil)
-(global-set-key (kbd "C-c C-d") #'helpful-at-point)
-
-;; make horizontal touchpad scrolling work (not tested on mac)
-(global-set-key (kbd "<triple-wheel-right>")
-  (lambda () (interactive) (scroll-right 2)))
-(global-set-key (kbd "<triple-wheel-left>")
-  (lambda () (interactive) (scroll-left 2)))
 
 (use-package which-key
   :init (which-key-mode)
@@ -249,7 +244,6 @@
     :keymaps '(normal)
     :prefix ","))
 
-
 (use-package hydra)
 
 (defhydra hydra-text-scale (:timeout 4)
@@ -266,11 +260,17 @@
 (use-package helpful
   :bind
   ([remap describe-command] . helpful-command)
-  ([remap describe-key] . helpful-key))
+  ([remap describe-key] . helpful-key)
+  :config
+  (global-set-key (kbd "C-c C-d") #'helpful-at-point))
 
 ;; setup avy like my hop.nvim setup
-(use-package avy)
-(evil-define-key 'normal 'global "s" 'evil-avy-goto-char)
+(use-package avy
+  :config
+  (evil-define-key 'normal 'global "s" 'evil-avy-goto-char)
+)
+
+(use-package deadgrep)
 
 ;; quick keymaps from vim
 (evil-define-key 'normal 'global ",b" 'switch-to-buffer)
@@ -278,7 +278,7 @@
 (evil-define-key 'normal 'global ",o" 'recentf-open-files)
 (evil-define-key 'normal 'global ",a" 'deadgrep)
 
-(defun self/org-mode-setup ()
+(defun jdr/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
   (visual-line-mode 1)
@@ -313,7 +313,7 @@
   )
 
 (use-package org
-  :hook (org-mode . self/org-mode-setup)
+  :hook (org-mode . jdr/org-mode-setup)
   :config
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-into-drawer t)
@@ -325,13 +325,13 @@
     '((sequence "TODO(t)" "IN-PROGRESS(p!)" "WAITING(w@/!)" "|" "DONE(d!)" "CANCELLED(c!)")))
   )
 
-(defun self/lsp-mode-setup ()
+(defun jdr/lsp-mode-setup ()
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-headerline-breadcrumb-mode))
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
-  :hook (lsp-mode . self/lsp-mode-setup)
+  :hook (lsp-mode . jdr/lsp-mode-setup)
   :init
   (setq lsp-keymap-prefix "C-l")  ;; TODO set this up so it's =SPC l=
   :config
@@ -367,13 +367,6 @@
 ;; (rune/leader-keys
 ;;     "la" '(projectile--find-file :which-key "code action"))
 
-;; Langauge configuration ------------------------------------------------------
-(use-package typescript-mode
-  :mode "\\.ts\\'\\|\\.tsx\\'"
-  :hook (typescript-mode . lsp-deferred)
-  :config
-  (setq typescript-indent-level 2))
-
 (use-package tree-sitter
   :ensure t
   :hook ((typescript-mode . tree-sitter-hl-mode)
@@ -386,10 +379,21 @@
   (tree-sitter-require 'tsx)
   (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-mode . tsx)))
 
+;; Langauge configuration ------------------------------------------------------
+(use-package typescript-mode
+  :mode "\\.ts\\'\\|\\.tsx\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
 (use-package nix-mode
   :mode "\\.nix\\'")
 
-(use-package magit)
+(use-package magit
+  :commands magit-status)
 
-;; TODO what does this do?
-(put 'scroll-left 'disabled nil)
+(use-package yasnippet
+  :config
+  (yas-global-mode 1)
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+)
